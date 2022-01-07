3a2d9c9bf2d99ee3d02c4a8cfdbea1e62ce852ea
Бобровский Дмитрий <ministrbob777@gmail.com>
2022-01-07 11:00:07 +0300

    create docker image

--- README.md
+++ README.md
@@ -1,7 +1,37 @@
 # MikrotikBackup
-Python program for backup Mikrotik
+Python program for backup Mikrotik with SSH. Program use config.conf (see config-example.conf).  
+Main idea: Create export backup on mikrotik. Download it and compare with previous version backup. If exist differences, additionally create binary backup and store both as archive history.  
+Now program store files as on local disk, but they can be saved anywhere if add this functionality.   
+(ATTENTION: export backup files .rsc contain passwords in clear text, therefore it is unsafe to store them in public repositories like github.com)  
 
-## Update requirements
+## Run (more convenient to use docker)
+```commandline
+# First of all, install the python 3.10 in accordance with the python installation documentation for the corresponding specific OS.
+cd /home/user/MikrotikBackup (or /opt/MikrotikBackup)
+git clone https://github.com/MinistrBob/MikrotikBackup.git .
+# Create venv or not.
+python3 -m pip install --upgrade pip
+python3 -m venv env
+source env/bin/activate
+python3 -m pip install -r requirements.txt
+python3 main.py
+```
+
+## Docker
+### Docker build (example for me)
+```commandline
+cd c:\MyGit\MikrotikBackup\
+git clone https://github.com/MinistrBob/MikrotikBackup.git .
+docker build . -t ministrbob/mikrotik-backup:latest
+docker push ministrbob/mikrotik-backup:latest
+```
+### Docker run (example for Windows)
+```commandline
+docker run --rm -it -e MIKROTIK_BACKUP_PATH="/backup" -v c:\!SAVE\backup\:/backup ministrbob/mikrotik-backup:latest
+```
+
+## For developer
+### Update requirements (on Windows)
 ```
 pip freeze | Out-File -Encoding UTF8 requirements.txt
 ```
--- main.py
+++ main.py
@@ -32,7 +32,57 @@ def compare_files(f1, f2):
                 return True
 
 
+def backup_mikrotik(curr_date, base_path, line):
+    # Work with mikrotik
+    print(f"Work with {line[0]} mikrotik")
+    mk = Mikrotik(line[0], line[1], line[2])
+    # Create export backup. Compare with previous.
+    # If exist differences, additionally create binary backup and store both.
+    # Binary backup restore: /system backup load name=<file_name>
+    mk.backup_export("current.rsc")
+    backup_folder = os.path.join(base_path, line[0])
+    # Create folder for backup
+    if not os.path.exists(backup_folder):
+        print(f"Create folder for backup {backup_folder}")
+        try:
+            os.mkdir(backup_folder)
+        except OSError as e:
+            print(f"ERROR: Can't create folder {backup_folder}:\n{e}")
+    # Copy local current.rsc file to previous.rsc
+    current_file = os.path.join(backup_folder, "current.rsc")
+    previous_file = os.path.join(backup_folder, "previous.rsc")
+    if os.path.exists(current_file):
+        try:
+            os.replace(current_file, previous_file)
+        except OSError as e:
+            print(f"ERROR: Can't rename current.rsc to previous.rsc:\n{e}")
+    # Download current.rsc locally
+    mk.download_file("current.rsc", current_file)
+    # Compare current and previous files
+    result = compare_files(current_file, previous_file)
+    # If exist differences, additionally create binary backup and store both
+    if result:
+        print(f"No changes. Nothing to work")
+    else:
+        print(f"There are changes. Making a backup")
+        backup_name = f"{line[0]}-{curr_date}"
+        # Copy local current.rsc to <backup_name>.rsc
+        backup_file = os.path.join(backup_folder, f"{backup_name}.rsc")
+        print(f"Copy {current_file} to {backup_file}")
+        try:
+            shutil.copy(current_file, backup_file)
+        except Error as e:
+            print(f"ERROR: Can't copy current.rsc:\n{e}")
+        # Create binary backup additionally
+        mk.backup_backup("current")
+        # Download current.backup locally
+        binary_backup_file = os.path.join(backup_folder, f"{backup_name}.backup")
+        mk.download_file("current.backup", binary_backup_file)
+    print()
+
+
 if __name__ == '__main__':
+    begin_time = datetime.datetime.now()
     curr_date = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
     base_path = os.getenv('MIKROTIK_BACKUP_PATH')
 
@@ -47,55 +97,10 @@ if __name__ == '__main__':
         config = csv.reader(f, delimiter=';')
         # headers = next(reader, None)
         for line in config:  # ['192.168.2.1', 'admin', '']
-            print(line)
+            # print(line)
             if line[0].startswith('#'):
                 continue
-            # Work with mikrotik
-            mk = Mikrotik(line[0], line[1], line[2])
+            backup_mikrotik(curr_date, base_path, line)
 
-            # Create export backup. Compare with previous.
-            # If exist differences, additionally create binary backup and store both.
-            # Binary backup restore: /system backup load name=<file_name>
-            mk.backup_export("current.rsc")
-            backup_folder = os.path.join(base_path, line[0])
-
-            # Create folder for backup
-            if not os.path.exists(backup_folder):
-                print(f"Create folder for backup {backup_folder}")
-                try:
-                    os.mkdir(backup_folder)
-                except OSError as e:
-                    print(f"ERROR: Can't create folder {backup_folder}:\n{e}")
-
-            # Copy local current.rsc file to previous.rsc
-            current_file = os.path.join(backup_folder, "current.rsc")
-            previous_file = os.path.join(backup_folder, "previous.rsc")
-            if os.path.exists(current_file):
-                try:
-                    os.replace(current_file, previous_file)
-                except OSError as e:
-                    print(f"ERROR: Can't rename current.rsc to previous.rsc:\n{e}")
-
-            # Download current.rsc locally
-            mk.download_file("current.rsc", current_file)
-
-            # Compare current and previous files
-            result = compare_files(current_file, previous_file)
-            # If exist differences, additionally create binary backup and store both
-            if result:
-                print(f"No changes. Nothing to work")
-            else:
-                print(f"There are changes. Making a backup")
-                backup_name = f"{line[0]}-{curr_date}"
-                # Copy local current.rsc to <backup_name>.rsc
-                backup_file = os.path.join(backup_folder, f"{backup_name}.rsc")
-                print(f"Copy {current_file} to {backup_file}")
-                try:
-                    shutil.copy(current_file, backup_file)
-                except Error as e:
-                    print(f"ERROR: Can't copy current.rsc:\n{e}")
-                # Create binary backup additionally
-                mk.backup_backup("current")
-                # Download current.backup locally
-                binary_backup_file = os.path.join(backup_folder, f"{backup_name}.backup")
-                mk.download_file("current.backup", binary_backup_file)
+    print(f"Program completed")
+    print(f"Total time spent: {datetime.datetime.now() - begin_time} sec.")
